dcc9dcc92cbe9d252a47c52b915a88075cefb618
tomoharu-ishibashi <biz.double.moon84@gmail.com>
2020-10-29 21:24:28 +0900

    create component

---
+++ src/components/MyButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Emit, Vue } from "vue-property-decorator";
+
+@Component
+export default class MyButton extends Vue {
+  private count = 0;
+
+  @Prop()
+  public greet?: string;
+
+  @Emit()
+  public click(count: number) {
+    console.log(count);
+  }
+
+  public onClick() {
+    alert(this.greet);
+    this.count++;
+    this.click(this.count);
+  }
+}
+</script>
