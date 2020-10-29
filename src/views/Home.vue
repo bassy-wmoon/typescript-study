be95f36ea1a27adaf9d2914ac55455edd6f0aa72
tomoharu-ishibashi <biz.double.moon84@gmail.com>
2020-10-29 21:25:30 +0900

    import new components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>{{ greetText }}</p>
+    <p>挨拶した回数：{{ count }}回</p>
+    <p v-if="isRegulars">いつもありがとうございます。</p>
+    <p>
+      <my-button :greet="greetText" @click="onMyButtonClicked">
+        おはこんばんちわ
+      </my-button>
+    </p>
+    <p>
+      <reset-button initialValue="はろー" v-model="greetText"> </reset-button>
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue, Watch } from "vue-property-decorator";
+import MyButton from "@/components/MyButton.vue";
+import ResetButton from "@/components/ResetButton.vue";
 
 @Component({
   components: {
-    HelloWorld
+    MyButton,
+    ResetButton
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private count = 0;
+  public greetText = "Hello";
+
+  public get isRegulars() {
+    return this.count >= 5;
+  }
+
+  @Watch("count")
+  public mt5Times() {
+    if (this.count == 10) {
+      alert("常連になりました");
+    }
+  }
+
+  public onMyButtonClicked(count: number) {
+    this.greetText = "こんにちは";
+    this.count = count;
+  }
+}
 </script>
