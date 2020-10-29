9f6eeb0b30694563599f04e4d71d5ef1fe211b4d
tomoharu-ishibashi <biz.double.moon84@gmail.com>
2020-10-29 21:25:04 +0900

    create component

---
+++ src/components/ResetButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="onClick">Reset</button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Emit, Vue } from "vue-property-decorator";
+
+@Component
+export default class ResetButton extends Vue {
+  private initialValue!: string;
+
+  @Prop()
+  public value!: string;
+
+  @Emit()
+  public input(value: string) {
+    console.log(value);
+  }
+
+  public created() {
+    console.log(this.value);
+    this.initialValue = this.value;
+  }
+
+  public onClick() {
+    this.input(this.initialValue);
+  }
+}
+</script>
